3715315a898fd61cf859c2aad4e8fa3199011b66
Nick Yeung <n.yeung91@gmail.com>
2023-03-09 20:58:15 +1300

    components uppercase

--- ai-personalised-learning-alpha/src/pages/nickprototype/inputuser.tsx
+++ ai-personalised-learning-alpha/src/pages/nickprototype/inputuser.tsx
@@ -1,6 +1,6 @@
 import { api } from "../../utils/api";
 import { useState } from "react";
-import Button from "../../components/button";
+import Button from "../../Components/button";
 import { nanoid } from "nanoid";
 import Link from "next/link";
 
