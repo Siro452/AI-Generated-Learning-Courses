e6cde0c4eea716a04b82b95d140fa1970a3a4e14
JordanR-img <jordanrobin18@gmail.com>
2023-03-13 11:27:10 +1300

    please keep tsconfig on the outer level, i know there is 2 but I'm getting the parsing error

--- ai-personalised-learning-alpha/src/Components/ContentCreator.tsx
+++
@@ -1,7 +0,0 @@
-export default function ContentCreator() {
-  return (
-    <div className=" flex h-40 items-start pl-40 ">
-      <h3 className=" text-purple-500 flex">AI Learning Content Creator</h3>
-    </div>
-  );
-}
--- ai-personalised-learning-alpha/src/pages/jordanprototype/index.tsx
+++ ai-personalised-learning-alpha/src/pages/jordanprototype/index.tsx
@@ -1,12 +1,14 @@
-import FileUpload from "../../components/fileupload";
+// import FileUpload from "../../components/fileupload";
 import React from "react";
 // import DataHandler from "../../components/DataHandler";
 import Upload from "./uploadInterface";
-import fileUpload from "../../components/fileupload";
+// import fileUpload from "../../components/fileupload";
 export default function JordanPrototype() {
   return (
     <div>
-   <FileUpload />
+   {/* <FileUpload /> */}
+
+   <div>fkdjskjfd</div>
     </div>
   );
 }
