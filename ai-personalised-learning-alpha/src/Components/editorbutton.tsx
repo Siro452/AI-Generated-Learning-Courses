49de7763aae2305db0c113bf39ec313626c192c9
Nick Yeung <n.yeung91@gmail.com>
2023-03-13 11:57:00 +1300

    added buttons on questions

---
+++ ai-personalised-learning-alpha/src/Components/editorbutton.tsx
@@ -0,0 +1,10 @@
+import AddIcon from "@mui/icons-material/Add";
+import Image from "next/image";
+
+export default function EditorButton(props) {
+  return (
+    <button className="rounded-full">
+      <AddIcon />
+    </button>
+  );
+}
--- ai-personalised-learning-alpha/src/pages/nickprototype/questioninput.tsx
+++ ai-personalised-learning-alpha/src/pages/nickprototype/questioninput.tsx
@@ -1,7 +1,9 @@
+import EditorButton from "../../components/editorbutton";
+
 export default function QuestionInput() {
   return (
     <div>
-      <div>
+      <div className="flex flex-col items-center justify-center ">
         <div className="mt-48 flex w-1/2 flex-col items-start justify-center">
           <img
             src="https://www.dacreed.com/static/Dacreed-logo-dark-f80888dcbd37d98ef1c3c84aabb5c049.png"
@@ -9,24 +11,35 @@ export default function QuestionInput() {
           ></img>
           <h3 className="text-[#5F00BA]">AI Learning Content Creator</h3>
         </div>
-        <div>
+        <div className="flex w-full flex-col items-center">
           <h2>Edit Your Course</h2>
 
-          <form>
+          <form className="flex w-1/2 flex-col items-center">
             <label>Course Title</label>
             <input
               type="text"
               name="courseTitle"
               placeholder="Enter/edit course title"
+              className="h-24"
             />
-            <label>Course Description</label>
+            <label className="self-start">Course Description</label>
             <input
               type="text"
               name="courseDescription"
               placeholder="Enter/edit course description"
+              className="h-24"
             />
             <label>Section 1</label>
+            <input
+              type="text"
+              name="section 1"
+              placeholder="Enter/edit section paragraph"
+              className="h-24"
+            ></input>
           </form>
+          <div>
+            <EditorButton />
+          </div>
         </div>
       </div>
     </div>
