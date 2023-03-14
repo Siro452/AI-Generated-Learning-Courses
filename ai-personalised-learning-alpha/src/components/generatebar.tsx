281bf9124d8d435c434adbddf8e88a595dcc0f4f
JordanR-img <jordanrobin18@gmail.com>
2023-03-14 14:40:55 +1300

    tailwind not looking too shabby

--- ai-personalised-learning-alpha/src/components/generatebar.tsx
+++ ai-personalised-learning-alpha/src/components/generatebar.tsx
@@ -1,16 +1,44 @@
-import Button from './generatebtn'
+// import Button from './generatebtn'
 
+// export default function generate(props: any) {
+//   return (
+//     <div className="rounded-3xl border-2">
+//       <input
+//         className="rounded-3xl"
+//         type="text"
+//         placeholder={props.placeholder}
+//       ></input>
+
+//       <div><Button title="Generate" /></div>
+//     </div>
+//   );
+// }
+
+// import Button from './generatebtn';
+
+// export default function generate(props: any) {
+//   return (
+//     <div className="flex rounded-3xl border-2">
+//       <input
+//         className="flex-grow rounded-l-3xl"
+//         type="text"
+//         placeholder={props.placeholder}
+//       />
+//       <Button title="Generate" className="rounded-r-3xl" />
+//     </div>
+//   );
+// }
+import Button from "./generatebtn";
 
 export default function generate(props: any) {
   return (
-    <div className="rounded-3xl border-2">
+    <div className="flex rounded-3xl">
       <input
-        className="rounded-3xl"
+        className="flex-grow rounded-full px-4 py-2 border-2 mr-2 "
         type="text"
         placeholder={props.placeholder}
-      ></input>
-
-      <div><Button title="Generate" /></div>
+      />
+      <Button title="Generate" />
     </div>
   );
 }
--- ai-personalised-learning-alpha/src/components/generatebtn.tsx
+++ ai-personalised-learning-alpha/src/components/generatebtn.tsx
@@ -14,5 +14,9 @@ export default function generate(props: {
     | ReactFragment
     | ReactPortal;
 }) {
-  return <span className="border-8 text-white text-xl rounded 3xl border-sky-400 rounded-r-full rounded-l-full bg-sky-400 px-6 py-2">{props.title}</span>;
+  return (
+    <span className="3xl cursor-pointer rounded rounded-full rounded-r-full rounded-l-full border-8 border-sky-400 bg-sky-400 px-6 py-2 py-2 px-4 text-xl text-white">
+      {props.title}
+    </span>
+  );
 }
