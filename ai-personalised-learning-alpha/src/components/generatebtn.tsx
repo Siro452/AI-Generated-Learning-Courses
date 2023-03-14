e6940c226a6969d97e4df550694902573940d49b
JordanR-img <jordanrobin18@gmail.com>
2023-03-14 14:04:10 +1300

    finished generator button tailwind

--- ai-personalised-learning-alpha/src/components/generatebtn.tsx
+++ ai-personalised-learning-alpha/src/components/generatebtn.tsx
@@ -14,5 +14,5 @@ export default function generate(props: {
     | ReactFragment
     | ReactPortal;
 }) {
-  return <span className="border-8">{props.title}</span>;
+  return <span className="border-8 text-white text-xl rounded 3xl border-sky-400 rounded-r-full rounded-l-full bg-sky-400 px-6 py-2">{props.title}</span>;
 }
