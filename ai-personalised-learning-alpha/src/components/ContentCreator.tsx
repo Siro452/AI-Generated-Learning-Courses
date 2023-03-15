6085072d2d89f135ef848557f45d681be8716319
JordanR-img <jordanrobin18@gmail.com>
2023-03-16 11:56:02 +1300

    fixed prisma error should work now

--- ai-personalised-learning-alpha/src/components/ContentCreator.tsx
+++ ai-personalised-learning-alpha/src/components/ContentCreator.tsx
@@ -1,4 +1,4 @@
-export default function ContentCreator():any {
+export default function ContentCreator() {
   return (
     <div className=" flex h-40 items-start pl-40 ">
       <h3 className=" text-purple-500 flex">AI Learning Content Creator</h3>
