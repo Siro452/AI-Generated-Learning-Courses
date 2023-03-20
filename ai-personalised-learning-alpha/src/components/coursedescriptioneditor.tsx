6c8cfb6748c68963c95b37d4d2a4c86c8ee666e3
Nick Yeung <n.yeung91@gmail.com>
2023-03-20 14:42:23 +1300

    alignment of text editor and character count

--- ai-personalised-learning-alpha/src/components/coursedescriptioneditor.tsx
+++ ai-personalised-learning-alpha/src/components/coursedescriptioneditor.tsx
@@ -9,18 +9,20 @@ export default function CourseDescriptionEditor() {
   };
 
   return (
-    <div className="my-6 flex w-3/4 flex-col rounded-xl border-l-[14px] border-[#DBE4FF] text-[#353535] drop-shadow ">
-      <input
-        type="text"
-        placeholder="Course description"
-        className="h-full w-full py-4 pl-2 text-2xl font-bold text-[#353535] placeholder:align-bottom placeholder:text-[#353535] "
-      ></input>
-      <input
-        type="text"
-        placeholder="Description Text"
-        className="border-t-2 border-[#D9D9D9] py-4 pl-2 placeholder:text-start placeholder:text-[#353535]"
-        onChange={characterCounter}
-      ></input>
+    <div className="flex w-full flex-col">
+      <div className="my-6 flex w-3/4 flex-col rounded-xl border-l-[14px] border-[#DBE4FF] text-[#353535] drop-shadow ">
+        <input
+          type="text"
+          placeholder="Course description"
+          className="h-full w-full py-4 pl-2 text-2xl font-bold text-[#353535] placeholder:align-bottom placeholder:text-[#353535] "
+        ></input>
+        <input
+          type="text"
+          placeholder="Description Text"
+          className="border-t-2 border-[#D9D9D9] py-4 pl-2 placeholder:text-start placeholder:text-[#353535]"
+          onChange={characterCounter}
+        ></input>
+      </div>
       <p className="self-end text-[#B6B6B6]">Characters : {charCount}</p>
     </div>
   );
