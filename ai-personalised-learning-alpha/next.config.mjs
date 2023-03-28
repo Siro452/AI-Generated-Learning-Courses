10abe01f10a6b302312dd67a85509903bccf19f9
Nick Yeung <nicholasye@missionreadyhq.com>
2023-03-28 15:15:35 +1300

    revert

--- ai-personalised-learning-alpha/next.config.mjs
+++ ai-personalised-learning-alpha/next.config.mjs
@@ -22,23 +22,7 @@ const config = {
   },
 };
 
-//enable cors
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "server/api/root.ts",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
-};
+
 
 
 
