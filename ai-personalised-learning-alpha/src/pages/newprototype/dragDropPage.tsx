53be77f85748b95180669a7cb8ed7e79eca2c979
JordanR-img <jordanrobin18@gmail.com>
2023-04-04 17:35:50 +1200

    making changes to background

--- ai-personalised-learning-alpha/src/pages/newprototype/dragDropPage.tsx
+++ ai-personalised-learning-alpha/src/pages/newprototype/dragDropPage.tsx
@@ -82,55 +82,61 @@ export default function createNewCourse() {
   return (
     <>
       {/* Section 3 --- User Input */}
-
-      <header className="items-left flex h-60 flex-col justify-between py-4">
-        {/* Logo */}
-        <div className="flex items-center">
-          <LogoContainer className="h-8" />
-        </div>
-
-        <span className="ml-12 text-2xl font-bold text-blue-800">
-          AI Learning Content Creator
-        </span>
-      </header>
-      <main className="mx-auto max-w-screen-lg">
-        {/* File upload and sliders */}
-        <section className="mt-30 flex flex-col items-center justify-center lg:flex-row">
-          {/* File upload */}
-          <div className=" h-full lg:w-1/2">
-            <FileUpload />
+      <div className="relative bg-white">
+      <div className="h-screen w-screen bg-gradient-to-r from-purple-500 via-indigo-500 to-violet-800">
+        <div className="absolute inset-0 transform inset-y-0 right-0 -skew-y-12 origin-top-left bg-white opacity-25 overflow-hidden"></div>
+        <header className="items-left flex h-60 flex-col justify-between py-4">
+          {/* Logo */}
+          <div className="flex items-center">
+            <LogoContainer className="h-8" />
           </div>
-          {/* Sliders */}
-          <div className="mt-[-2rem] w-full justify-center pl-10 lg:w-1/2">
-            <div className="pb-10 text-center text-2xl font-black font-extrabold drop-shadow-md lg:text-left">
-              {/* Title */}
-              <span className="mr-20 whitespace-nowrap border-2 border-amber-500  text-xl text-indigo-800">
-                <HeaderContainer
-                  smallTitle=""
-                  largeTitle="Dial your preferences"
-                  flexDirection="flex "
+
+          <span className="ml-12 text-2xl font-bold text-blue-800">
+            AI Learning Content Creator
+          </span>
+        </header>
+        <main className="mx-auto max-w-screen-lg">
+          {/* File upload and sliders */}
+          <section className="mt-30 flex flex-col items-center justify-center lg:flex-row">
+            {/* File upload */}
+            <div className=" h-full lg:w-1/2">
+              <FileUpload />
+            </div>
+            {/* Sliders */}
+            <div className="mt-[-7rem] w-full justify-center pl-10 lg:w-1/2">
+              <div className="pb-10 text-center text-2xl font-black font-extrabold drop-shadow-md lg:text-left">
+                {/* Title */}
+                <span className="whitespace-nowrap text-xl text-indigo-800">
+                  <HeaderContainer
+                    smallTitle=""
+                    largeTitle="Dial your preferences"
+                    flexDirection=""
+                    alignment=""
+                    mx="mx-11"
+                  />
+                </span>
+
+                {/* Filters */}
+                <Filters />
+              </div>
+
+              {/* Button */}
+              <div
+                className="flex justify-center lg:justify-end"
+                onClick={generate}
+              >
+                <Button
+                  text={"Next"}
+                  type={"button"}
+                  href={"/jordanprototype/loadingpage"}
+                  className="w-60"
                 />
-              </span>
-
-              {/* Filters */}
-              <Filters />
+              </div>
             </div>
-
-            {/* Button */}
-            <div
-              className="flex justify-center lg:justify-end"
-              onClick={generate}
-            >
-              <Button
-                text={"Next"}
-                type={"button"}
-                href={"/jordanprototype/loadingpage"}
-                className="w-60"
-              />
-            </div>
-          </div>
-        </section>
-      </main>
+          </section>
+        </main>
+      </div>
+      </div>
     </>
   );
 }
